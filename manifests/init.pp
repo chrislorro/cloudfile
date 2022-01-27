bf08dbcfeddb2cc8a4214a0e9971b3e454233128
Christopher Lawrence <chris.lawrence@puppet.com>
2022-01-27 17:08:24 +0000

    testing cloudfile

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
-# @summary Download archived packages from various locations and optionally ocal install
+# @summary Download archived packages from various locations and optionally local install.
 #
 # A module to download application files from cloud filestores or http/https hosted packages,
-# currently compatable with AWS and Azure cloud providers
+# currently compatable with AWS and Azure cloud providers. Package installation features will
+# be added in future releases, including running a template to install custom applications
+# that cannot use the `package` resource type.
 #
 # @param application
 #   The application name that is being installed
@@ -20,7 +22,10 @@
 #   The cloud platform that is used to host the application package
 #
 # @params install_package
-#   Optinal parameter to install the package (this option will be enhanced in the next release)
+#   Optional parameter to install the package (this option will be enhanced in the next release)
+#
+# @params install_options
+#   Optional parameter to pass installation options to the package installer
 # @params token
 #   Optional parameter for passing a token to the package URL tested on AZ for now, 
 #    token is expected if IAM is configured on the platform
@@ -29,28 +34,48 @@
 #   Optional parameter for the AWS region, if this is not set for,
 #   windows agents the package uses a default deployed with the sdk
 #
-# @example retrieve an application call invader from s3 storage
-#   class cloudfile::getfile {
+# @example retrieve an application call invader from s3 storage to linux
+#   class cloudfile {
 #     app_name     => 'invader',
 #     package_file => 'invader.tar.gz',
-#     package_uri  => 's3://chrislaw',
+#     package_uri  => 's3://chrislorro',
 #     extract      => true,
 #     cloud_type   => local_s3,
-#     aws_region   => undef,
+#     aws_region   => eu-west-2,
 #   }
 #
+# @example retrieve an application call invader from s3 storage and install the application
+#   class cloudfile {
+#     app_name        => 'mcafee',
+#     package_file    => 'McAfee.zip',
+#     package_uri     => 'https://chrislorro.blob.core.windows.net/puppet,
+#     extract         => true,
+#     cloud_type      => local_az,
+#     token           => 'sp=rwd&st=2022-01-27T11:46[…]sig=2ytxxRcpND1Khs4UgUL1tfMxOwHsZMMit'
+#     package_name    => McAfee.exe,
+#     install_options => [ '/SILENT', '/INSTALL=AGENT']
+#   }
 class cloudfile (
-  String             $application      = 'invader',
-  String             $package_file     = 'invader.zip',
-  String             $package_uri      = 's3://clawpupbuck',
-  Boolean            $extract          = true,
+  String             $application     = undef,
+  String             $package_file    = undef,
+  String             $package_uri     = undef,
+  Boolean            $extract         = true,
   Enum[ 'local_az',
         'local_s3',
-        'std_http' ] $cloud_type       = 'local_s3',
-  Optional[String]   $package_name     = undef,
-  Optional[String]   $token            = undef,
-  Optional[String]   $aws_region       = 'eu-west-2',
+        'std_http' ] $cloud_type      = undef,
+  Optional[String]   $package_name    = undef,
+  Optional[String]   $token           = undef,
+  Optional[String]   $aws_region      = undef,
+  Optional[Array]    $install_options = undef,
+
 ) {
+
+  $temp_dir = $facts['kernel'] ? {
+    default   => '/var/tmp',
+    'windows' => 'C:/Windows/TEMP'
+  }
+
+  $install_dir = "${temp_dir}/${application}${$package_name}"
 
   include archive
 
@@ -61,5 +86,14 @@ class cloudfile (
     access_key => $token,
     cloud_type => $cloud_type,
     aws_region => $aws_region,
+  }
+
+  if $package_name {
+    package { $package_name:
+      ensure          => 'installed',
+      source          => $install_dir,
+      install_options => $install_options,
+      require         => Cloudfile::Getfile[$package_file]
+    }
   }
 }
