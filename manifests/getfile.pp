a8e38d6dc37017369e5da2e8b96601734feed525
Christopher Lawrence <chris.lawrence@puppet.com>
2022-01-27 10:35:31 +0000

    testing cloudfile

---
+++ manifests/getfile.pp
@@ -0,0 +1,147 @@
+# @summary Accepts parameters for downloadind and extracting files.
+#
+# == Definition: cloudfile::getfile
+#
+# Parameters:
+#
+# - *$pkg_uri: $package_uri
+# - *$app_name: $applicarion
+# - *$extract: default value true.
+# - *$ensure: default value true.
+# - *$cloud_type: default value std_http
+# - *$access_key: optional param default undef
+# - *$aws_region: optional param default is undef
+#
+#  cloudfile::getfile { 'mycloudapp.2.0.tar.gz':
+#    app_name   => 'mycloudapp',
+#    cloud_type => 'azure',
+#    access_key => '<azure token key>'
+#    pkg_uri    => 'http://clouduri/blob/distro/v2.0',
+#  }
+#
+# Example usage:
+#  cloudfile::getfile { 'mycloudapp.2.0.tar.gz':
+#    app_name   => 'mycloudapp',
+#    cloud_type => 'aws',
+#    extract    => false,
+#    pkg_uri    => 'http://clouduri/blob/distro/v2.0',
+#    aws_region => 'eu-west2',
+#  }
+define cloudfile::getfile (
+
+  String                    $pkg_uri      = $package_uri,
+  String                    $app_name     = $application,
+  Boolean                   $extract      = true,
+  Enum['present', 'absent'] $ensure       = present,
+  String                    $cloud_type   = undef,
+  Optional[String]          $access_key   = undef,
+  Optional[String]          $aws_region   = $aws_region,
+
+) {
+
+  $temp_dir = $facts['kernel'] ? {
+    default   => '/var/tmp',
+    'windows' => 'C:/Windows/TEMP'
+  }
+
+  $_extract_dir = "${temp_dir}/${app_name}"
+  $_pkg_inst    = "${_extract_dir}/${title}"
+
+  $_pkg_src_uri = $cloud_type ? {
+    default   => "${pkg_uri}/${title}",
+    'local_az' => "${pkg_uri}/${title}?${access_key}",
+  }
+
+  file { $_extract_dir:
+    ensure => directory,
+  }
+
+  case $cloud_type {
+    default: {
+      archive { $_pkg_inst:
+        ensure       => $ensure,
+        extract      => $extract,
+        source       => $_pkg_src_uri,
+        extract_path => $_extract_dir,
+        creates      => $_pkg_inst,
+      }
+    }
+
+    local_s3: {
+
+      $archive_params = $facts['kernel'] ? {
+        'Linux' => { path        => "${_extract_dir}/awscli-bundle.zip",
+                      source       => 'https://s3.amazonaws.com/aws-cli/awscli-bundle.zip',
+                      extract      => true,
+                      extract_path => '/opt',
+                      creates      => '/opt/awscli-bundle/install',
+                      cleanup      => true,
+                    },
+        'windows'   => { path             => "${_extract_dir}/awscliv2.msi",
+                      source           => 'https://awscli.amazonaws.com/AWSCLIV2.msi',
+                      creates          => 'C:/Program Files/Amazon/AWSCLIV2',
+                      download_options => ['--region', $aws_region],
+                    }
+      }
+
+      archive { 'Get AWS CLI':
+        ensure => present,
+        *      => $archive_params
+      }
+
+      if $facts['osfamily'] == 'windows' {
+
+        package { 'AWS Command Line Interface v2':
+          ensure          => 'installed',
+          source          => 'C:/Windows/TEMP/invader/awscliv2.msi',
+          install_options => [ '/qn'],
+          require         => Archive['Get AWS CLI'],
+          notify          => Exec[$_pkg_inst]
+        }
+
+      } else {
+
+        exec { 'install_aws_cli':
+          command     => '/opt/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws',
+          refreshonly => true,
+          subscribe   => Archive['Get AWS CLI'],
+          notify      => Exec[$_pkg_inst]
+        }
+      }
+
+      $aws_options = '--no-sign-request'
+
+      $aws_cmd = $facts['osfamily'] ? {
+        default => '/usr/local/bin',
+        windows => 'C:\\Program Files\\Amazon\\AWSCLIV2'
+      }
+
+      exec { $_pkg_inst:
+        cwd       => $_extract_dir,
+        command   => "aws s3 cp ${_pkg_src_uri} ${_extract_dir} ${aws_options}",
+        path      => $aws_cmd,
+        logoutput => true,
+        creates   => $_pkg_inst,
+      }
+
+      if $extract == 'true' {
+        archive { $_pkg_inst:
+          ensure       => $ensure,
+          extract      => true,
+          extract_path => $_extract_dir,
+          require      => Exec[$_pkg_inst],
+        }
+      }
+    }
+
+    local_az: {
+      archive { $_pkg_inst:
+        ensure       => $ensure,
+        extract      => $extract,
+        source       => $_pkg_src_uri,
+        extract_path => $_extract_dir,
+        creates      => $_pkg_inst,
+      }
+    }
+  }
+}
---
+++ manifests/init.pp
@@ -0,0 +1,63 @@
+# @summary Download archived packages from various locations to prepare for local installation
+#
+# A module to download application files from cloud filestores or http/https hosted packages,
+# currently compatable with AWS and Azure cloud providers
+#
+# @param application
+#   The application name that is being installed
+#
+# @param package_file
+#   The package name that will be downloaded from cloud storage
+#
+# @params package_uri
+#   The URL of the cloud storage where the package is stored
+#
+# @params extract
+#   set this parameter to extract the package from an archive file,
+#   it defaults to true because we expect a compressed file
+#
+# @params cloud_type
+#   The cloud platform that is used to host the application package
+#
+# @params az_token
+#   Optional parameter for retreive Azure filestore packages, 
+#    token is expected if IAM is configured on the platform
+#
+# @params aws_region
+#   Optional parameter for the AWS region, if this is not set for,
+#   windows agents the package uses a default deployed with the sdk
+#
+# @example retrieve an application call invader from s3 storage
+#   class cloudfile::getfile {
+#     app_name     => 'invader',
+#     package_file => 'invader.tar.gz',
+#     package_uri  => 's3://chrislaw',
+#     extract      => true,
+#     cloud_type   => std_http,
+#     az_token     => local_s3,
+#     aws_region   => undef,
+#   }
+#
+class cloudfile (
+  String              $application        = undef,
+  String              $package_file       = undef,
+  String              $package_uri        = undef,
+  Boolean             $extract            = true,
+  Enum[ 'local_az',
+        'local_s3',
+        'std_http' ]  $cloud_type         = std_http,
+  Optional[string]    $az_token           = undef,
+  Optional[String]    $aws_region         = undef,
+) {
+
+  include archive
+
+  profile::apps::cloud::storage_bucket { $package_file:
+    pkg_uri    => $package_uri,
+    app_name   => $application,
+    extract    => $extract,
+    access_key => $az_token,
+    cloud_type => $cloud_type,
+    aws_region => $aws_region,
+  }
+}
