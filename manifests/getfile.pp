5b768d2242082d29a16d3c74beb45b864a45db66
Christopher Lawrence <chris.lawrence@puppet.com>
2022-01-27 14:31:52 +0000

    testing cloudfile

--- manifests/getfile.pp
+++ manifests/getfile.pp
@@ -106,7 +106,7 @@ define cloudfile::getfile (
           command => '/opt/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws',
           creates => '/usr/local/bin/aws',
           require => Archive['Get AWS CLI'],
-          notify  => Exec[$_pkg_inst]
+          notify  => Archive[$_pkg_inst]
         }
       }
 
